6d36be22a2cd5ad4a341fe2270fde2f3fdacb458
doniyorjonjaloliddinov <don2003.01.09@gmail.com>
2022-12-25 00:04:57 +0500

    server

--- config/settings.py
+++ config/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-w3i*5!$2d0fpstkpl)1ivimuo(jtbr889z0=+)nv=4kci4yq89
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
